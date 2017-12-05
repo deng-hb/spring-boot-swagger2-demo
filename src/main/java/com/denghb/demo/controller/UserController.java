548df30df3bd059078f230c788f5dd1bc1a026e0
denghb <i@denghb.com>
2017-12-05 15:25:11 +0800

    完善demo

--- src/main/java/com/denghb/demo/controller/UserController.java
+++ src/main/java/com/denghb/demo/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.denghb.demo.controller;
 
 import com.denghb.demo.domain.User;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,9 +42,9 @@ public class UserController {
         return new ArrayList<User>(users.values());
     }
 
-    @ApiOperation(value = "获取指定ID用户信息", notes = "")
+    @ApiOperation(value = "获取指定ID用户信息", notes = "<pre>id:用户ID，\nname:姓名</pre>" ,response = User.class)
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long",paramType = "path")
     @ResponseBody
     public User get(@PathVariable("id") Long id) {
         return users.get(id);
